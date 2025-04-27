6e62455ab79419ee001a6f765e4cf5522bfe14ba
Dan Adrian Mirea <damirea84@gmail.com>
2025-04-27 08:13:07 +0300

    remove setwindowposition call on toggle fullscreen

--- src/game.cpp
+++ src/game.cpp
@@ -569,7 +569,6 @@ void Game::UpdateUI()
         {
             fullscreen = false;
             ToggleBorderlessWindowed();
-            SetWindowPosition(minimizeOffset, minimizeOffset);
         }
         else
         {
